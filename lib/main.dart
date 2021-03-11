825f1d7e417c548fbe19cf22c949343df123597a
= <=>
2021-03-12 06:57:46 +0700

    mengatur posisi

--- lib/main.dart
+++ lib/main.dart
@@ -37,79 +37,107 @@ class _MyAppState extends State<MyApp> {
           //judul dari project
           title: Text("Kalkulator Sehat"),
         ),
-        body: Column(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                //dropdown untuk memilih jenis kelamin
-                Container(
-                  margin: EdgeInsets.all(5),
-                  child: DropdownButton<String>(
-                    items: listJenis.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    isExpanded: true,
-                    value: _jenis,
-                    onChanged: _dropdownChange,
-                  ),
-                ),
-                //form untuk memasukkan usia
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Masukkan Usia',
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  //dropdown untuk memilih jenis kelamin
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: DropdownButton<String>(
+                      items: listJenis.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      isExpanded: true,
+                      value: _jenis,
+                      onChanged: _dropdownChange,
                     ),
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    controller: inputSuhu,
-                    keyboardType:
-                        TextInputType.numberWithOptions(decimal: true),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: TextFormField(
-                    decoration:
-                        InputDecoration(hintText: 'Masukkan Tinggi badan'),
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    controller: inputTB,
-                    keyboardType:
-                        TextInputType.numberWithOptions(decimal: true),
+                  //form untuk memasukkan usia
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Masukkan Usia',
+                      ),
+                      controller: inputSuhu,
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                    ),
                   ),
-                ),
-                //form untuk memasukkan tinggi badan
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: TextFormField(
-                    decoration:
-                        InputDecoration(hintText: 'Masukkan Berat badan'),
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    controller: inputBB,
-                    keyboardType:
-                        TextInputType.numberWithOptions(decimal: true),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                    child: TextFormField(
+                      decoration:
+                          InputDecoration(hintText: 'Masukkan Tinggi badan'),
+                      controller: inputTB,
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                    ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(15),
-                  height: 40,
-                  width: 180,
-                  child: RaisedButton(
-                    onPressed: () {},
+                  //form untuk memasukkan tinggi badan
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                    child: TextFormField(
+                      decoration:
+                          InputDecoration(hintText: 'Masukkan Berat badan'),
+                      controller: inputBB,
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(80, 20, 80, 30),
+                    height: 40,
+                    width: 180,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Hitung',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      textColor: Colors.white,
+                      color: Colors.pink,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 250, 20),
                     child: Text(
-                      'Hitung',
-                      style: TextStyle(fontSize: 17),
+                      "BMI = ",
+                      style: TextStyle(fontSize: 18),
                     ),
-                    textColor: Colors.white,
-                    color: Colors.pink,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 240, 20),
+                    child: Text(
+                      "BMR = ",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 140, 20),
+                    child: Text(
+                      "Berat badan ideal = ",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
