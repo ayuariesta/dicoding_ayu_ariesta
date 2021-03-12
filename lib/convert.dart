66f6b7fb205628ac1f971cfbb1a95bbbdd6a4903
= <=>
2021-03-12 09:11:24 +0700

    onpressed diisi sesuai parameter

--- lib/convert.dart
+++ lib/convert.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Convert extends StatelessWidget {
-  const Convert({Key key, @required String title})
+  const Convert({Key key, @required String title, @required Function hitung})
       : _title = title,
+        _hitung = hitung,
         super(key: key);
 
   final String _title;
+  final Function _hitung;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.fromLTRB(15, 5, 3, 7),
+      margin: EdgeInsets.fromLTRB(15, 20, 3, 7),
       //digunakan untuk melakukan perhitungan
       child: RaisedButton(
-        onPressed: () {},
+        onPressed: _hitung,
         child: Text(
           _title,
           style: TextStyle(fontSize: 17),
