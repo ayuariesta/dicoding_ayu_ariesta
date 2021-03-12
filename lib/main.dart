a013dc935716de256e54cf5be4c09243fb2631f2
= <=>
2021-03-12 07:40:45 +0700

    menambahkan rumus bmr

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,51 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var listJenis = ["Laki-laki", "Perempuan"];
   String _jenis = "Perempuan";
-  var inputSuhu = new TextEditingController();
+  var inputUsia = new TextEditingController();
   var inputTB = new TextEditingController();
   var inputBB = new TextEditingController();
+
+  //state
+  double _inputUsia = 0;
+  double _inputTinggi = 0;
+  double _inputBerat = 0;
+  double _usia = 0;
+  double _tinggi = 0;
+  double _berat = 0;
+  double _resultBMI = 0;
+  double _resultBMR = 0;
+  double _resultIdeal = 0;
 
   _dropdownChange(String value) {
     setState(() {
       _jenis = value;
+    });
+  }
+
+  // ignore: unused_element
+  _rumusBMI() {
+    setState(() {
+      _inputTinggi = double.parse(inputTB.text);
+      _inputBerat = double.parse(inputBB.text);
+      _resultBMI = (_inputBerat * 10000) / (_inputBerat * _inputBerat);
+    });
+  }
+
+  _rumusBMR() {
+    setState(() {
+      _inputTinggi = double.parse(inputTB.text);
+      _inputBerat = double.parse(inputBB.text);
+      _inputUsia = double.parse(inputUsia.text);
+      if (_jenis == "Perempuan")
+        _resultBMR = 66.5 +
+            (13.7 * _inputBerat) +
+            (5 * _inputTinggi) -
+            (6.8 * _inputUsia);
+      else
+        _resultBMR = 655 +
+            (9.6 * _inputBerat) +
+            (1.8 * _inputTinggi) -
+            (4.7 * _inputUsia);
     });
   }
 
@@ -65,7 +103,7 @@ class _MyAppState extends State<MyApp> {
                       decoration: InputDecoration(
                         hintText: 'Masukkan Usia',
                       ),
-                      controller: inputSuhu,
+                      controller: inputUsia,
                       keyboardType:
                           TextInputType.numberWithOptions(decimal: true),
                     ),
@@ -74,8 +112,8 @@ class _MyAppState extends State<MyApp> {
                     margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
                     //untuk melakukan input tinggi badan
                     child: TextFormField(
-                      decoration:
-                          InputDecoration(hintText: 'Masukkan Tinggi badan'),
+                      decoration: InputDecoration(
+                          hintText: 'Masukkan Tinggi badan (cm)'),
                       controller: inputTB,
                       keyboardType:
                           TextInputType.numberWithOptions(decimal: true),
@@ -119,21 +157,21 @@ class _MyAppState extends State<MyApp> {
                     //untuk menampilkan hasil dari perhitungan
                     margin: EdgeInsets.fromLTRB(0, 0, 250, 20),
                     child: Text(
-                      "BMI = ",
+                      "BMI = " + '$_resultBMI',
                       style: TextStyle(fontSize: 18),
                     ),
                   ),
                   Container(
                     margin: EdgeInsets.fromLTRB(0, 0, 240, 20),
                     child: Text(
-                      "BMR = ",
+                      "BMR = " + '$_resultBMR',
                       style: TextStyle(fontSize: 18),
                     ),
                   ),
                   Container(
                     margin: EdgeInsets.fromLTRB(0, 0, 140, 20),
                     child: Text(
-                      "Berat badan ideal = ",
+                      "Berat badan ideal = " + '$_resultIdeal',
                       style: TextStyle(fontSize: 18),
                     ),
                   ),
